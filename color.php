7e7ca7ee0320af36f30736942b157eb8c93b0cf7
Welling Guzman <hola@wellingguzman.com>
2017-11-09 00:03:00 -0400

    oops

--- color.php
+++ color.php
@@ -1,5 +1,14 @@
 <?php
 
+/**
+ * Dump a image data
+ *
+ * @param int $w Width
+ * @param int $h Height
+ * @param int $r Red (0-255)
+ * @param int $g Green (0-255)
+ * @param int $b Blue (0-255)
+ */
 function create_image($w, $h, $r, $g, $b)
 {
   $img = imagecreate($w, $h);
@@ -7,6 +16,6 @@ function create_image($w, $h, $r, $g, $b)
 
   header('Content-Type: image/png');
   imagepng($img);
-  imagecolorallocate($background);
+  imagecolordeallocate($img, $background);
   imagedestroy($img);
 }
